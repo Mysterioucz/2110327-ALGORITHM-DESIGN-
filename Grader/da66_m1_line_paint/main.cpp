bcf919d69cb606d46fb9ea1017263a33ce18b221
Mysterioucz <chatrinyoonchalard@gmail.com>
2025-01-30 20:04:34 +0700

    Grader done: line paint

--- Grader/da66_m1_line_paint/main.cpp
+++ Grader/da66_m1_line_paint/main.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 
 int main(){
+    int n,a,b;
+    cin >> n;
+
+    vector<pair<int,int>> v,res;
+    while(n--){
+        cin >> a >> b;
+        v.push_back({a,b});
+    }
+    sort(v.begin(),v.end());
+    res.push_back(v[0]);
+    for(auto e:v){
+        a = res[res.size() - 1].first;
+        b = res[res.size() - 1].second;
+        if(e.first - b >= 0){
+            if(e.first - b == 1 || e.first - b == 0){
+                res[res.size() - 1] = {a,e.second};
+            }else{
+                res.push_back(e);
+            }
+        }else{
+            res[res.size() - 1] = {a,max(b,e.second)};
+        }
+    }
+    for(auto e:res){
+        cout << e.first << " " << e.second << " ";
+    }
 
     return 0;
 }
