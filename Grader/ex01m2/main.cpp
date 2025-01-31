4f73e1509abb6bdcafb2ecc83945cc716d7dd92d
Mysterioucz <chatrinyoonchalard@gmail.com>
2025-01-31 14:35:32 +0700

    grader done: inversion

--- Grader/ex01m2/main.cpp
+++ Grader/ex01m2/main.cpp
@@ -1,45 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int count_inversion(const vector<int> &a,const vector<int> &b){
-    int ans = 0;
-    for(int i = 0;i < a.size();i++){
-        int j = 0;
-        while(j < b.size() && a[i] > b[j]){
-            j++;
-        }
-        ans += j;
-    }
 
-    return ans;
+int merge(){
+
+    return 0;
+}
+
+void merge_sort(vector<int> &v,int start, int stop,vector<int> &tmp){
+
 }
 
 int main(){
-
-    // cout << count_inversion({9},{1,2,3,4,5});
+    ios_base::sync_with_stdio(false); cin.tie(0);
     // need to solve using merge sort
-    unordered_map<int,vector<int>> idx;
-    vector<int> data;
-
     int n,tmp;
     cin >> n;
-
+    vector<int> data(n);
     for(int i = 0;i < n;i++){
-        cin >> tmp;
-        if(!idx.count(tmp)){
-            data.push_back(tmp);
-        }
-        idx[tmp].push_back(i);
+        cin >> data[i];
     }
-    sort(data.begin(),data.end());
-
-    int aj = 0;int ans = 0;
-    while(aj < data.size() - 1){
-        for(int ai = aj + 1;ai < data.size();ai++){
-        ans += count_inversion(idx[data[aj]],idx[data[ai]]);
+    // sort(data.begin(),data.end());
+    int ans = 0;
+    for(int i = 0;i < data.size() - 1;i++){
+        for(int j = i+1;j < data.size();j++){
+            if(data[i] > data[j]) ans++;
         }
-        aj++;
     }
+    
     cout << ans;
 
     return 0;
