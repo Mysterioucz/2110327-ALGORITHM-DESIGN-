60e81e4bc1e08efb912ef81e31f593e1c0e31d29
Mysterioucz <chatrinza@gmail.com>
2025-01-31 19:25:32 +0700

    Grader Done: tripple sum

--- Grader/a58_q1_triple/main.cpp
+++ Grader/a58_q1_triple/main.cpp
@@ -6,32 +6,27 @@ int main(){
     int n, m, tmp;
     cin >> n >> m;
     vector<int> data(n);
-    set<int> s;
     for(int i = 0;i < n;i++){
         cin >> data[i];
-        s.insert(data[i]);
     }
     while(m--){
         cin >> tmp; 
         bool found = false;
         for(int i = 0;i < data.size() && !found;i++){
-            if(data[i] >= tmp) break;
             //using two pointer
-            for(int j = i + 1;j < data.size() && !found;j++){
-                if(data[i] + data[j] >= tmp) break;
-                if(s.find(tmp - data[i] - data[j]) != s.end()) found = true;
+            int left = i + 1;int right = data.size() - 1;
+            while(left < right && !found){
+                int total = data[i] + data[left] + data[right];
+                if(total > tmp){
+                    right--;
+                }else if(total < tmp){
+                    left++;
+                }else{
+                    found = true;
+                }
 
-                // for(int k = j + 1;k < data.size() && !found;k++){
-                //     if(data[i] + data[j] + data[k]> tmp) break;
-                //     else if(data[i] + data[j] + data[k] == tmp) found = true;
-                // }
             }
         }
-        // for(int i = 0;i < data.size() && !found;i++){
-        //     for(int j = (s.lower_bound({tmp - data[i],-1}))->second;j >= 0;j--){
-
-        //     }
-        // }
         if(found) cout << "YES" <<"\n";
         else cout << "NO" << "\n";
     }
