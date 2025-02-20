2b28fae3bb97a173e1f9f419f85ffb7c05c4c360
Mysterioucz <chatrinza@gmail.com>
2025-02-20 15:22:14 +0700

    Grader Done: Lawn mowing

--- Grader/a64_q1_lawn_mowing/main.cpp
+++ Grader/a64_q1_lawn_mowing/main.cpp
@@ -1,29 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+// using long long = long long;
 
-ll calcPrice(const vector<ll> &grass,const vector<ll> &psa,ll k,ll start,ll bud){
-    ll l = start + 1;ll r = grass.size() - 1;
+long long calcPrice(const vector<long long> &grass,const vector<long long> &psa,long long k,long long start,long long bud){
+    long long l = start;long long r = grass.size() - 1;
+    long long cost;
     while(l < r){
-        ll mid = (l + r + 1)/2;
-        ll cost = (psa[mid] - psa[start]) + (r-l + 1)*k;
+        long long mid = (l + r + 1)/2;
+        cost = (psa[mid] - psa[start - 1]) + (mid - start + 1)*k;
         if(cost > bud){
             r = mid - 1;
         }else if(cost < bud){
             l = mid + 1;
         }else if(cost == bud){
-            return psa[mid] - psa[start];
+            return psa[mid] - psa[start - 1];
         }
     }
-    return psa[r] - psa[start];
+    cost = (psa[r] - psa[start - 1]) + (r-start + 1)*k;
+    while(cost > bud && r > 0){
+        r--;
+        cost = (psa[r] - psa[start - 1]) + (r-start + 1)*k;
+    }
+    return psa[r] - psa[start - 1];
 }
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL);
-    ll n,m,k,start,bud;
+    long long n,m,k,start,bud;
     cin >> n >> m >> k;
-    vector<ll> grass(n + 1);
-    vector<ll> psa(n + 1);
+    vector<long long> grass(n + 1);
+    vector<long long> psa(n + 1);
     psa[0] = 0;
     for(int i = 1;i < n + 1;i++){
         cin >> grass[i];
@@ -31,7 +37,7 @@ int main(){
     }
     while(m--){
         cin >> start >> bud;
-        cout << calcPrice(grass,psa,k,start,bud) << "\n";
+        cout << calcPrice(grass,psa,k,start + 1,bud) << "\n";
     }
 
     return 0;
