7a3fcd683385ab0dc26ffdcb8ae38f7a0cfa7b65
Mysterioucz <chatrinza@gmail.com>
2025-02-22 22:27:45 +0700

    Pareto on progess

--- Grader/a60b_q2_pareto/main.cpp
+++ Grader/a60b_q2_pareto/main.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> pareto(int l,int r,const vector<pair<int,int>> &v){ // Vector v need to be sorted by X Cord
+    vector<int> res;
+    if(r - l == 1){
+        if(v[r].first > v[l].first && v[r].second > v[l].second){
+            res.push_back(l);
+        }else if(v[r].first < v[l].first && v[r].second < v[l].second){
+            res.push_back(r);
+        }
+        return res;
+    }
+
+    int mid = (l + r)/2;
+    vector<int> paretoLeft = pareto(l,mid,v);
+    vector<int> paretoRight = pareto(mid + 1,r,v);
+    
+
+}
+
 int main(){
 
     return 0;
