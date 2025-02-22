c72c5499089da2fcfec393488ea2fdaa2cfeac31
Mysterioucz <chatrinyoonchalard@gmail.com>
2025-02-22 18:00:32 +0700

    Grader Matrix Recur

--- Grader/a62_q1a_recur/main.cpp
+++ Grader/a62_q1a_recur/main.cpp
@@ -1,7 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void recur_matrix(vector<vector<int>> &v,int a, int b,int top,int bottom,int left,int right){
+    if(a == 0){ // terminate case
+        v[top][left] = b;
+    }
+    recur_matrix(v,a-1,b,top,(bottom + 1)/2,left,(right + 1)/2); // fill the top left of matrix H(a,b)
+    recur_matrix(v,a-1,b - 1,top,(bottom + 1)/2,(right + 1)/2,right); // fill the top right of matrix H(a,b)
+    recur_matrix(v,a-1,b + 1,(bottom + 1)/2,bottom,left,(right + 1)/2); // fill the bottom left of matrix H(a,b)
+    recur_matrix(v,a-1,b,(bottom + 1)/2,bottom,(right + 1)/2,right); // fill the bottom left of matrix H(a,b)
+
+}
+
 int main(){
-    
+    int a,b;
+    cin >> a >> b;
+    vector<vector<int>> data(1 << a,vector<int>(1 << a));
+    recur_matrix(data,a,b,0,1 << a,0,1<< a);
+    for(const vector<int> &r:data){
+        for(const int &e:r){
+            cout << e << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 }
