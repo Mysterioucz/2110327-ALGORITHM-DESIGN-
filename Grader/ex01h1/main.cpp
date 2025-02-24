6e6dca7382d8a88283ed5075cb8ea12548de07f5
Mysterioucz <chatrinza@gmail.com>
2025-02-24 18:18:05 +0700

    Grader Done: Self Describing Sequence

---
+++ Grader/ex01h1/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<long long> dp(3,0); // {end_index,value}
+    dp[1] = 1;
+    dp[2] = 3;
+    for(int i = 3;i < 1e6;i++){
+        auto it = (lower_bound(dp.begin(),dp.end(),i));
+        long long idx = (it - dp.begin());
+        dp.push_back(dp[i-1] + idx);
+    }
+    while(n--){
+        long long tmp;
+        cin >> tmp;
+        cout << lower_bound(dp.begin(),dp.end(),tmp) - dp.begin() << "\n";
+    }
+    // cout << dp[3] << ", " << dp[4] << ", " << dp[5];
+    return 0;
+}
--- Grader/ex03e1_lcs/main.cpp
+++ Grader/ex03e1_lcs/main.cpp
@@ -6,16 +6,19 @@ int main(){
     cin >> a >> b;
     // Create a.size() * b.size() table which lable for subseq start with i and end with j
     vector<vector<int > > data(a.size() + 1,vector<int>((b.size() + 1),0));
+    string res = "";
     for(int i = 1;i <= a.size();i++){
         for(int j = 1;j <= b.size();j++){
             if(a[i - 1] == b[j - 1]){
                 data[i][j] = data[i - 1][j - 1] + 1;
+                res += a[i-1];
             }else{
                 data[i][j] = max(data[i][j - 1],data[i - 1][j]);
             }
         }
     }
     cout << data[a.size()][b.size()] << "\n";
+    cout << res << "\n";
     
     return 0;
 }
