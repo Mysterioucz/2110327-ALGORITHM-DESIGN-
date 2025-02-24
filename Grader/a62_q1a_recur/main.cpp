f1afe858bce1c72e3041f529351c114e8ba5468d
Mysterioucz <chatrinza@gmail.com>
2025-02-24 20:46:33 +0700

    Grader Done: Recur Matrix

--- Grader/a62_q1a_recur/main.cpp
+++ Grader/a62_q1a_recur/main.cpp
@@ -4,12 +4,13 @@ using namespace std;
 void recur_matrix(vector<vector<int>> &v,int a, int b,int top,int bottom,int left,int right){
     if(a == 0){ // terminate case
         v[top][left] = b;
+        return;
     }
-    recur_matrix(v,a-1,b,top,(bottom + 1)/2,left,(right + 1)/2); // fill the top left of matrix H(a,b)
-    recur_matrix(v,a-1,b - 1,top,(bottom + 1)/2,(right + 1)/2,right); // fill the top right of matrix H(a,b)
-    recur_matrix(v,a-1,b + 1,(bottom + 1)/2,bottom,left,(right + 1)/2); // fill the bottom left of matrix H(a,b)
-    recur_matrix(v,a-1,b,(bottom + 1)/2,bottom,(right + 1)/2,right); // fill the bottom left of matrix H(a,b)
-
+    recur_matrix(v,a-1,b,top,(top + bottom + 1)/2,left,(left + right + 1)/2); // fill the top left of matrix H(a,b)
+    recur_matrix(v,a-1,b - 1,top,(top + bottom + 1)/2,(left + right + 1)/2,right); // fill the top right of matrix H(a,b)
+    recur_matrix(v,a-1,b + 1,(top + bottom + 1)/2,bottom,left,(left + right + 1)/2); // fill the bottom left of matrix H(a,b)
+    recur_matrix(v,a-1,b,(top + bottom + 1)/2,bottom,(left + right + 1)/2,right); // fill the bottom left of matrix H(a,b)
+    
 }
 
 int main(){
