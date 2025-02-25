268d0defb351f6ef89a7abfe556c6004807c2e38
Mysterioucz <chatrinza@gmail.com>
2025-02-25 12:25:14 +0700

    Barcode need to fix

--- Grader/ex02m2_barcode/dp.cpp
+++ Grader/ex02m2_barcode/dp.cpp
@@ -9,24 +9,31 @@ int main(){
     for(int i = 1;i < n + 1;i++){
         for(int j = 1;j < m + 1;j++){
             // initial condition of recurrence relation
-            dp[i][j][0] = 1;
+            if(i <= j) dp[i][j][0] = 1;
         }
     }
     // dp[1][1][0] = 1;
-    for(int i = 1;i < n;i++){
-        for(int j = 1;j < m;j++){
-            for(int k = 0;k < c;k++){
+    for(int i = 1;i < n + 1;i++){
+        for(int j = 1;j < m + 1;j++){
+            for(int k = 1;k < c + 1;k++){
                 // initial condition of recurrence relatio
-                // if(i == j+k){
-                //     dp[i][j][k] = dp[i][j][k-1] + dp[i][j-1][k];
-                // }else if(i < j + k){
-                //     dp[i][j][k] = dp[i][j-1][k];
-                // }
+                if(i - j == k){
+                    dp[i][j][k] = dp[i-1][j-1][k] + dp[i-1][j][k-1];
+                }else {
+                    dp[i][j][k] = dp[i][j-1][k];
+                }
                 
-                dp[i+1][j+1][k]++;
-                dp[i+1][j][k+1]++;
             }
         }
+    }
+    for(int i = 0;i < n + 1;i++){
+        for(int j = 0;j < m + 1;j++){
+            for(int k = 0;k < c + 1;k++){
+                cout << dp[i][j][k] << " ";
+            }
+            cout << "\n";
+        }
+        cout << "\n";
     }
 
     cout << dp[n][m][c] << "\n";
